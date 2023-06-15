1d61f89f4b001def5f3039d7b40a24e820269424
lrhhz <sq6qtl5o2frnv.hazwoper@aleeas.com>
2023-06-15 19:25:23 +0800

    修改upgrader获取能量逻辑

--- dist/role.upgrader.js
+++ dist/role.upgrader.js
@@ -7,6 +7,7 @@ const roleUpgrader = {
             creep.memory.upgrading = true;
             creep.say("升级")
         }
+
         if (creep.memory.upgrading) {
             if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: "#cc2424"}});
@@ -14,20 +15,20 @@ const roleUpgrader = {
         } else {
             const sources = creep.room.find(FIND_SOURCES);
             const targets = creep.room.find(FIND_DROPPED_RESOURCES)
-            /*const targets = creep.room.find(FIND_STRUCTURES, {
+            const containers = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return structure.structureType === STRUCTURE_CONTAINER
                 }
-            });*/
-            //if (targets.length > 0 && targets[0].store[RESOURCE_ENERGY] > 0)
+            });
             if (targets.length > 0) {
-                /*if(creep.withdraw(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: "#ff0099"}});
-                    creep.say("从container中获取能量");
-                }*/
                 creep.moveTo(targets[0], {visualizePathStyle: {stroke: "#ff0099"}});
                 creep.pickup(targets[0]);
                 creep.say("从掉落能量中获取能量");
+            } else if (containers.length > 0 && containers[0].store[RESOURCE_ENERGY] > 0) {
+                if(creep.withdraw(containers[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(containers[0], {visualizePathStyle: {stroke: "#ff0099"}});
+                    creep.say("从container中获取能量");
+                }
             } else {
                 if(creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(sources[1], {visualizePathStyle: {stroke: "#ff0099"}});
